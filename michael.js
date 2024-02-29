653307ca6d6c9e73dfa0f8aebcfc7d42c564909d
SnakeJazzzz <michael2506@icloud.com>
2024-02-29 13:51:10 -0600

    cambio en el archivo

--- michael.js
+++ michael.js
@@ -5,3 +5,18 @@ function factorial(n) {
   }
   return n * factorial(n - 1);
 }
+//cambio para demosntracion
+
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+}
